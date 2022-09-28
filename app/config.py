857d7581e1c8bd0d151c774c8ac1e0ea15dc2d91
Ganesh Sali <37493900+GaneshSali@users.noreply.github.com>
2022-09-28 15:06:36 +0530

    Init Repo

---
+++ app/config.py
@@ -0,0 +1,4 @@
+#----------Database MongoUri-----------
+
+MongoUri="mongodb://localhost/test?retryWrites=true"
+# MongoUri="mongodb+srv://ganesh:h3E3bJi6QiVdZ9wu@cg-cluster.a3zdw.mongodb.net/chainguardians?retryWrites=true&w=majority&ssl=true"
